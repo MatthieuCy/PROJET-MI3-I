48d41eba7ec183ed0414af4f1356e92d63c8e750
MatthieuCy <matthieu.dublanc@gmail.com>
2025-12-08 10:36:14 +0100

    Update avl_usine.c

--- avl_usine.c
+++ avl_usine.c
@@ -1,4 +1,126 @@
 #include "AVL_USINE.H"
+void lecture_ecriture_csv1()
+{
+    const char* fichier = "c-wildwater_v3.dat";
+    FILE *fic = fopen(fichier, "r");
+
+    if (fic == NULL){
+        perror("fichier introuvable ou erreur d'ouverture");
+        exit(1);
+    }
+
+    FILE *fic2 = fopen("vol_max.dat", "w");
+    if (fic2 == NULL){
+        perror("fichier de sortie introuvable");
+        exit(1);
+    }
+
+    fprintf(fic2, "identifiant_usine;volume_max (k.m3.year-1)\n");
+
+    char line[800];
+    
+    char c1[50];      
+    char usine[50];        
+    char c3[50];        
+    char volume_max[50];  
+    char c5[50];        
+
+    while (fgets(line, sizeof(line), fic))
+    {
+        sscanf(line, "%49[^;];%49[^;];%49[^;];%49[^;];%49[^\n]",
+               c1, usine, c3, volume_max, c5);
+
+        
+        if (strcmp(c1, "-") == 0 && strcmp(c3, "-") == 0 && strcmp(c5, "-") == 0)
+        {
+            fprintf(fic2, "%s;%s\n", usine, volume_max);
+        }
+    }
+
+    fclose(fic);
+    fclose(fic2);
+}
+void lecture_ecriture_csv2(){
+        
+    FILE *fic = fopen("c-wildwater_v3.dat", "r");
+    FILE *fic2 = fopen("vol_captation.txt", "w");
+
+    if (fic == NULL || fic2 == NULL){
+        perror("Erreur ouverture fichier");
+        exit(1);
+        }
+
+    fprintf(fic2, "identifiant_usine;volume_source (k.m3.year-1)\n");
+
+    char line[800];
+    char c1[100], source[100], usine[100], volume[100], fuite[100];
+
+    while (fgets(line, sizeof(line), fic)){
+        sscanf(line, "%99[^;];%99[^;];%99[^;];%99[^;];%99[^\n]",
+               c1, source, usine, volume, fuite);
+
+        if (strcmp(c1, "-") == 0 && strcmp(volume, "-") != 0 && strcmp(fuite, "-") != 0){
+            fprintf(fic2, "%s;%s\n", usine, volume);
+        }
+    }
+
+    fclose(fic);
+    fclose(fic2);
+}
+
+void lecture_ecriture_csv3()
+{
+    const char* fichier = "c-wildwater_v3.dat";
+    FILE *fic = fopen(fichier, "r");
+
+    if (fic == NULL){
+        perror("fichier introuvable ou erreur d'ouverture");
+        exit(1);
+    }
+
+    FILE *fic2 = fopen("vol_traitement.tmp", "w");
+    if (fic2 == NULL){
+        perror("fichier de sortie introuvable");
+        exit(1);
+    }
+
+    fprintf(fic2, "identifiant_usine;volume_reel (k.m3.year-1)\n");
+
+    char line[800];
+    char c1[50], source[50], usine[50], volume[50], fuite[50];
+
+    double v, f, v_reel;
+
+    while (fgets(line, sizeof(line), fic))
+    {
+                
+        sscanf(line, "%49[^;];%49[^;];%49[^;];%49[^;];%49[^\n]",
+               c1, source, usine, volume, fuite);
+
+      
+        if (strcmp(c1, "-") == 0 && strcmp(volume, "-") != 0 && strcmp(fuite, "-") != 0)
+        {
+            v = atof(volume);  
+            f = atof(fuite);    
+
+            v_reel = v * (1 - (f / 100.0));
+
+            fprintf(fic2, "%s;%.2f\n", usine, v_reel);
+        }
+    }
+
+    fclose(fic);
+    fclose(fic2);
+}
+int maxi(int a, int b){
+    if(a>=b)    return a;
+    return b;
+}
+
+int mini(int a, int b){
+    if(a<=b)    return a;
+    return b;
+}
 
 
 Usine creer_usine(const char *id, int capacite){
