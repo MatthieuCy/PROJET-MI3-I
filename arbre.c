597df79d0ba859dca890ba8a01afd0dad995c7c4
MatthieuCy <matthieu.dublanc@gmail.com>
2025-12-15 15:39:23 +0100

    Update arbre.c

--- arbre.c
+++ arbre.c
@@ -1,23 +1,4 @@
-typedef struct Chainon_Troncon {
-    double fuite_pct;                
-    struct Arbre_Noeud *cible;          
-    struct Chainon_Troncon *suivant;    
-} Chainon_Troncon;
 
-
-typedef struct Arbre_Noeud {
-    char *id;                           
-    Chainon_Troncon *troncons_aval;    
-} Arbre_Noeud;
-
-
-typedef struct Arbre_Graphe_AVL {
-    char *id;                           
-    Arbre_Noeud *noeud;                 
-    int hauteur;
-    struct Arbre_Graphe_AVL *gauche;
-    struct Arbre_Graphe_AVL *droite;
-} Arbre_Graphe_AVL;
 
 int maxi(int a, int b){
     if(a>=b)    return a;
