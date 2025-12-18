d5c4c6c6ab3c2b9d161914ab44c5ddd166f6bacf
Ibrahim75cy <ibrahim.fridjine@gmail.com>
2025-12-18 22:06:39 +0100

    Update avl.h

--- avl.h
+++ avl.h
@@ -113,7 +113,7 @@ void supprimer_troncons(Chainon_Troncon *tete);
 void supprimer_arbre_noeud(Arbre_Noeud *n);
 void avl_graphe_suppression(Arbre_Graphe_AVL *racine);
 void supprimer_noeuds_depuis_avl(Arbre_Graphe_AVL *racine);
-void supprimer_graphe_complet(Arbre_Graphe_AVL *racine_avl);
+void librerer_graphe_complet(Arbre_Graphe_AVL *racine_avl);
 
 
 // PROTOTYPES - AVL_USINE.C 
