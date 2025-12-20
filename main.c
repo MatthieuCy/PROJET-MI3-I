9bc34a1756e531aeae295f99a20d49c6eeb6ff9f
Ibrahim75cy <ibrahim.fridjine@gmail.com>
2025-12-20 16:23:35 +0100

    Update main.c

--- main.c
+++ main.c
@@ -1,45 +1,53 @@
 #include "avl.h"
 
-
 int main(int argc, char *argv[]) {
-    if (argc < 4) {
-        fprintf(stderr, "Usage: %s <mode> <fichier_entree> <option_ou_id>\n", argv[0]);
-        return 1; // Code erreur positif requis par le PDF
+    // Shell appelle : ./wildwater <mode> <fichier_entree> <option_ou_id> <fichier_sortie> donc 5 pour  (le nom du prog + 4 paramètres)
+    if (argc < 5) {
+        fprintf(stderr, "Usage: %s <mode> <fichier_entree> <option_ou_id> <fichier_sortie>\n", argv[0]);
+        return 1; 
     }
 
     char *mode = argv[1];
-    char *fichier_entree = argv[2]; // Devient dynamique
-    char *param = argv[3];
+    char *fichier_entree = argv[2];
+    char *param = argv[3];         // Pour histo : "max", "src" ou "real"  et Pour leaks : "ID_station"
+    char *fichier_sortie = argv[4]; // Le nom du fichier .dat à créer
 
+    // hisotgramme
     if (strcmp(mode, "histo") == 0) {
-        // On passe le nom du fichier à la fonction de lecture
         AVL_Usine *racine = lire_donnees_et_construire_avl(fichier_entree);
         if (!racine) return 2;
 
-        if (generer_histogramme(racine, param) != 0) {
+        // Génération de l'histogramme
+        if (generer_histogramme(racine, fichier_sortie, param) != 0) {
             liberer_avl_usine(racine);
             return 3;
         }
+        
         liberer_avl_usine(racine);
     } 
+    
+    // leaks
     else if (strcmp(mode, "leaks") == 0) {
         AVL_Usine *racine_usine = lire_donnees_et_construire_avl(fichier_entree);
         if (!racine_usine) return 2;
-        // On passe aussi le fichier au graphe
+
         Graphe_Global *graphe = construire_graphe_distribution(fichier_entree);
         if (!graphe) {
-           liberer_avl_usine(racine_usine);
-           return 3;
+            liberer_avl_usine(racine_usine);
+            return 3;
         }
-        
-        // calculer_rendement_distribution doit écrire "ID;-1.00" si ID non trouvé
-        double res = calculer_rendement_distribution(param, racine_usine, graphe, "leaks_history.dat");
+    
+        double res = calculer_rendement_distribution(param, racine_usine, graphe, fichier_sortie);
         
         liberer_avl_usine(racine_usine);
         liberer_graphe_complet(graphe);
         
-        if (res < 0) return 4; // Erreur si l'ID n'existe pas
-    } else {
+        if (res < 0) return 4; 
+    } 
+    
+    // autre
+    else {
+        fprintf(stderr, "Erreur : Mode '%s' non reconnu.\n", mode);
         return 5;
     }
 
