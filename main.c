eb348d2d1d1e95e7abd7a2bb8c1a4358e95f84e0
Ibrahim75cy <ibrahim.fridjine@gmail.com>
2025-12-21 09:19:10 +0100

    Update main.c

--- main.c
+++ main.c
@@ -14,10 +14,22 @@ int main(int argc, char *argv[]) {
 
     // hisotgramme
     if (strcmp(mode, "histo") == 0) {
+        
+        // construit l'AVL des usines et on calcule les volumes (src ou max)
         AVL_Usine *racine = lire_donnees_et_construire_avl(fichier_entree);
         if (!racine) return 2;
 
-        // Génération de l'histogramme
+        // Si le mode est 'real'
+        if (strcmp(param, "real") == 0) {
+            Graphe_Global *graphe = construire_graphe_distribution(fichier_entree);
+            if (graphe) {
+                //  parcourt usines AVL + calcule volume réel via le graphe
+                calculer_tous_les_volumes_reels(racine, graphe);
+                liberer_graphe_complet(graphe);
+            }
+        }
+
+        // Génération de l'histogramme 
         if (generer_histogramme(racine, fichier_sortie, param) != 0) {
             liberer_avl_usine(racine);
             return 3;
@@ -28,15 +40,18 @@ int main(int argc, char *argv[]) {
     
     // leaks
     else if (strcmp(mode, "leaks") == 0) {
+        // Chargement  usines pour  volumes de base (src)
         AVL_Usine *racine_usine = lire_donnees_et_construire_avl(fichier_entree);
         if (!racine_usine) return 2;
 
+        // Construction du graphe de distribution pour le calcul des pertes
         Graphe_Global *graphe = construire_graphe_distribution(fichier_entree);
         if (!graphe) {
             liberer_avl_usine(racine_usine);
             return 3;
         }
     
+        // Calcule  rendement d'une station précise et écrit dans le fichier
         double res = calculer_rendement_distribution(param, racine_usine, graphe, fichier_sortie);
         
         liberer_avl_usine(racine_usine);
@@ -53,3 +68,4 @@ int main(int argc, char *argv[]) {
 
     return 0; // Succès
 }
+
